5ebce9a1bb5a11667eafafdf43448ebef114d56f
Yirong Wang <yw5490@nyu.edu>
2025-03-11 19:24:40 -0400

    added App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from './App';
+import { homeHeader } from './App';
+
+describe('App', () => {
+  it('renders nav and home', async () => {
+    render(<App />);
+
+    await screen.findByRole('heading');
+    await screen.findAllByRole('listitem');
+
+    expect(screen.getByRole('heading')).toHaveTextContent('Welcome to Journal System');
+    
+    expect(screen.getAllByRole('listitem')).toHaveLength(6);
+  });
+
+  it('switches to about page', async () => {
+    render(<App />);
+
+    userEvent.click(screen.getByText('About'));
+
+    expect(screen.getByRole('heading', { level: 1 }))
+      .toHaveTextContent('About Our Journal System')
+  });
 });
